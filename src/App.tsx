a45b9e4055f4e8ac305600b3210cc486de88201e
PagiY <pagiyap003@gmail.com>
2023-09-13 22:41:11 +0800

    Created resizeable grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,56 @@
+import { useState } from 'react';
+import Grid from './components/Grid';
+// import { Canvas } from './Canvas';
+import './App.css';
+
 function App() {
+  const [cellCountX, setCellCountX] = useState(50);
+  const [cellCountY, setCellCountY] = useState(50);
+
+  const rows: React.ReactNode[] = [];
+  const cols: React.ReactNode[] = [];
+
+  const createCells = () => {
+    for(let i = 0; i < cellCountY; i++) {
+      rows.push(<Grid />);
+    }
+    for(let i = 0; i < cellCountX; i++) {
+      cols.push(<tr key={i}>{rows}</tr>);
+    }
+    return cols;
+  }
+
   return (
     <>
-      hello
+      <table cellSpacing={0}>
+        {
+          createCells()
+        }
+      </table>
+      <form>
+        <input
+          type="range"
+          min={10}
+          max={50}
+          step={1}
+          value={cellCountX}
+          onChange={(e) => {
+            setCellCountX(Number(e.target.value));
+          }}
+        />
+        <input
+          type="range"
+          min={10}
+          max={50}
+          step={1}
+          value={cellCountY}
+          onChange={(e) => {
+            setCellCountY(Number(e.target.value));
+          }}
+        />
+      </form>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Grid.tsx
@@ -0,0 +1,7 @@
+import './Grid.css';
+
+const Grid = () => {
+  return <td className="cell"></td>
+};
+
+export default Grid;
