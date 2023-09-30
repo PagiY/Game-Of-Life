2f3594a13d7a1bf43fca086697e2bfa484417932
PagiY <pagiyap003@gmail.com>
2023-09-30 22:42:14 +0800

    Working game of life logic, now playable

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import { useState, useEffect, useCallback } from 'react';
 
 import './App.css';
 
-const ROWS = 20;
-const COLUMNS = 20;
+const ROWS = 10;
+const COLUMNS = 10;
+
+const neighbors = [
+  [0, -1],    // up
+  [0, 1],     // down
+  [1, 0],     // right
+  [-1, 0],    // left
+  [-1, -1],   // upper-left
+  [1, -1],    // upper-right
+  [-1, 1],    // lower-left
+  [1, 1],     // lower-right
+];
 
 function App() {
   // create a 2D array of states for each cell
@@ -15,17 +26,71 @@ function App() {
     return r;
   });
 
+  // is user playing state
   const [isPlaying, setIsPlaying] = useState(false);
 
+  // game logic
   const playing = useCallback(() => {
-    console.log('hey!');
-  }, []);
+    // tempGrid is used for saving the state of new grid temporarily
+    const tempGrid = JSON.parse(JSON.stringify(grid)) // create deep copy...
 
+    for(let i = 0; i < ROWS; i++) {
+      for(let j = 0; j < COLUMNS; j++) {
+        // live cell
+        if (grid[i][j] === 1) {
+
+          let liveNeighbors = 0;
+
+          neighbors.forEach(([x,y]) => {
+            const neighborX = i+x;
+            const neighborY = j+y;
+            if ((neighborX >= 0 && neighborX < ROWS) && (neighborY >= 0 && neighborY < COLUMNS)) {
+              if(grid[neighborX][neighborY] === 1){
+                liveNeighbors++;
+              }
+            }
+          })
+          /**
+           * Any live cell with fewer than two live neighbours dies, as if by underpopulation. 
+           * Any live cell with two or three live neighbours lives on to the next generation.
+           * Any live cell with more than three live neighbours dies, as if by overpopulation.
+          */
+          // die condition
+          if (liveNeighbors < 2 || liveNeighbors > 3) {
+            tempGrid[i][j] = 0;
+          }
+        // dead cell
+        } else {
+  
+          let liveNeighbors = 0;
+
+          neighbors.forEach(([x,y]) => {
+            const neighborX = i+x;
+            const neighborY = j+y;
+            if ((neighborX >= 0 && neighborX < ROWS) && (neighborY >= 0 && neighborY < COLUMNS)) {
+              if(grid[neighborX][neighborY] === 1){
+                liveNeighbors++;
+              }
+            }
+          })
+          // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+          // live condition
+          if (liveNeighbors === 3) {
+            tempGrid[i][j] = 1;
+          }
+        }
+
+      }
+    }
+    setGrid(tempGrid);
+  }, [grid]);
+
+  // run the game if playing
   useEffect(() => {
     if (!isPlaying) {
       return;
     }
-    const loop = setInterval(playing, 1000);
+    const loop = setInterval(playing, 100);
     return () => clearInterval(loop);
   }, [isPlaying, playing]);
 
@@ -48,7 +113,7 @@ function App() {
                     }}
                     onClick={() => {
                       const tempGrid = [...grid];
-                      tempGrid[i][k] = 1;
+                      tempGrid[i][k] === 1 ? tempGrid[i][k] = 0 : tempGrid[i][k] = 1;
                       setGrid(tempGrid);
                     }}
                   />
@@ -59,7 +124,6 @@ function App() {
           ))
         }
       </table>
-      {console.log('rerender')}
       <button
         onClick={() => setIsPlaying(isPlaying ? false : true)}
       >
